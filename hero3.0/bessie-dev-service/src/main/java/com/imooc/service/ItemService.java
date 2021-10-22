4bf30283a05b4942357a5137ddcfd5798b9b49d0
Bessie <bchen372@gatech.edu>
2021-10-22 14:09:12 +0800

    3,2
    查询商品信息

--- hero3.0/bessie-dev-service/src/main/java/com/imooc/service/ItemService.java
+++ hero3.0/bessie-dev-service/src/main/java/com/imooc/service/ItemService.java
@@ -1,4 +1,44 @@
 package com.imooc.service;
 
+import com.imooc.pojo.Items;
+import com.imooc.pojo.ItemsImg;
+import com.imooc.pojo.ItemsParam;
+import com.imooc.pojo.ItemsSpec;
+
+import java.util.List;
+
 public interface ItemService {
+    /**
+     * 根据商品id获取商品
+     * @param itemId
+     * @return
+     */
+    public Items queryItemById(String itemId);
+
+    /**
+     * 根据商品id获取商品图片
+     * @param itemId
+     * @return
+     */
+    public List<ItemsImg> queryItemImgList(String itemId);
+
+    /**
+     * 根据商品id获取商品规格
+     * @param itemId
+     * @return
+     */
+    public List<ItemsSpec> queryItemSpecList(String itemId);
+
+    /**
+     * 根据商品id获取商品参数
+     * @param itemId
+     * @return
+     */
+    public ItemsParam queryItemParam(String itemId);
+
+    /**
+     * 获取评价个数
+     * @param itemId
+     */
+    public void queryCommentCounts(String itemId);
 }
--- hero3.0/bessie-dev-service/src/main/java/com/imooc/service/impl/ItemServiceImpl.java
+++ hero3.0/bessie-dev-service/src/main/java/com/imooc/service/impl/ItemServiceImpl.java
@@ -1,7 +1,21 @@
 package com.imooc.service.impl;
 
+import com.imooc.mapper.ItemsImgMapper;
+import com.imooc.mapper.ItemsMapper;
+import com.imooc.mapper.ItemsParamMapper;
+import com.imooc.mapper.ItemsSpecMapper;
 import com.imooc.pojo.Items;
+import com.imooc.pojo.ItemsImg;
+import com.imooc.pojo.ItemsParam;
+import com.imooc.pojo.ItemsSpec;
 import com.imooc.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
 
 /**
  * @program: bessie-dev
@@ -9,6 +23,85 @@ import com.imooc.service.ItemService;
  * @author: Bessie
  * @create: 2021-10-22 12:34
  **/
+@Service
 public class ItemServiceImpl implements ItemService {
 
+    @Autowired
+    private ItemsMapper itemsMapper;
+
+    @Autowired
+    private ItemsImgMapper itemsImgMapper;
+
+    @Autowired
+    private ItemsSpecMapper itemsSpecMapper;
+
+    @Autowired
+    private ItemsParamMapper itemsParamMapper;
+
+    /**
+     * 根据商品id获取商品
+     *
+     * @param itemId
+     * @return
+     */
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Items queryItemById(String itemId) {
+        return itemsMapper.selectByPrimaryKey(itemId);
+    }
+
+    /**
+     * 根据商品id获取商品图片
+     *
+     * @param itemId
+     * @return
+     */
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<ItemsImg> queryItemImgList(String itemId) {
+        Example itemsImgExp = new Example(ItemsImg.class);
+        Example.Criteria criteria = itemsImgExp.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+        return itemsImgMapper.selectByExample(itemsImgExp);
+    }
+
+    /**
+     * 根据商品id获取商品规格
+     *
+     * @param itemId
+     * @return
+     */
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<ItemsSpec> queryItemSpecList(String itemId) {
+        Example itemsSpecExp = new Example(ItemsSpec.class);
+        Example.Criteria criteria = itemsSpecExp.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+        return itemsSpecMapper.selectByExample(itemsSpecExp);
+    }
+
+    /**
+     * 根据商品id获取商品参数
+     *
+     * @param itemId
+     * @return
+     */
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public ItemsParam queryItemParam(String itemId) {
+        Example itemsParamExp = new Example(ItemsParam.class);
+        Example.Criteria criteria = itemsParamExp.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+        return itemsParamMapper.selectOneByExample(itemsParamExp);
+    }
+
+    /**
+     * 获取评价个数
+     *
+     * @param itemId
+     */
+    @Override
+    public void queryCommentCounts(String itemId) {
+
+    }
 }
